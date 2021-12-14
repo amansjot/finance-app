e7cbbf2e81f5a74d4a9a8468120273158522f532
amansjot <41126421+amansjot@users.noreply.github.com>
2021-12-14 18:53:23 -0500

    Updated view_spending() function

--- main.py
+++ main.py
@@ -11,17 +11,17 @@ def init(time: int):
     print("R to record spendings/earnings, V to view spendings/earnings, M to make recommendations, Q to quit")
     action = ""
     while action not in ["R", "V", "M", "Q"]:
-        action = input("What action would you like to perform? ")
+        action = input("What action would you like to perform? ").upper()
     if action == "R":
         ans = ""
         while ans not in ["S", "E"]:
-            ans = input("Spendings (S) or Earnings (E)? ")
+            ans = input("Spendings (S) or Earnings (E)? ").upper()
         if ans == "S":
-            record_spendings(conn)
+            record_spending(conn)
         elif ans == "E":
-            record_earnings()
+            record_earning(conn)
     elif action == "V":
-        view_spendings()
+        view_spending(conn)
     elif action == "M":
         make_rec()
     elif action == "Q":
@@ -42,6 +42,11 @@ def create_table(conn):
     conn.commit()
 
 def record_spendings(conn):
+    """
+    A function for the user to input data about a spending
+    :param conn: connection to the SQL database
+    :return: n/a (prints success)
+    """
     cursor = conn.cursor()
 
     print("-----")
@@ -50,7 +55,7 @@ def record_spendings(conn):
 
     spending = input("Date of spending: ")
     expense = input("Title of expense: ")
-    amount = input("Amount in $: ")
+    amount = input("Amount: $")
 
     cursor.execute("insert into expenses values (?, ?, ?)", (spending, expense, amount))
     conn.commit()
@@ -61,9 +66,44 @@ def record_spendings(conn):
 
     init(1)
 
+def view_spending(conn):
+    """
+        A function for the user to see their total spending/earning
+        :param conn: connection to the SQL database
+        :return: n/a (prints spending data)
+        """
+    cursor = conn.cursor()
+
+    print("-----")
+    print("Your Spending")
+    print("-----")
+
+    cursor.execute("select * from expenses")
+    data = cursor.fetchall()
+
+    for expense in data:
+        date = expense[0]
+        title = expense[1]
+        amount = '$' + str('{:,.2f}'.format(expense[2]))
+        print(amount + " for " + title + " on " + date)
+
+    print("-----")
+
+    total = 0
+    for expense in data:
+        amount = expense[2]
+        total += amount
+
+    print("Total Spending: $" + str(total))
+    print("-----")
+
+    init(1)
+
+
 def get_posts(conn):
     cursor = conn.cursor()
     cursor.execute("select * from expenses")
     print(cursor.fetchall())
+
 
 init(0)
