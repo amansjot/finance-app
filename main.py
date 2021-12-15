77a2584437203312514b6838bb87fb44c921b7f2
amansjot <41126421+amansjot@users.noreply.github.com>
2021-12-14 22:20:12 -0500

    Update main.py
    Fixed small bugs and errors

--- main.py
+++ main.py
@@ -2,13 +2,14 @@ import sqlite3 as db
 import sys
 
 def init(time: int):
+    conn = db.connect("expenses.db")
 
     if time == 0:
         print("Hello! Welcome to your personal Finance Tracker.")
-        conn = db.connect("expenses.db")
+        print("-----")
         create_table(conn)
 
-    print("R to record spendings/earnings, V to view spendings/earnings, M to make recommendations, Q to quit")
+    print("R to record spendings/earnings, V to view spendings/earnings, or Q to quit")
     action = ""
     while action not in ["R", "V", "M", "Q"]:
         action = input("What action would you like to perform? ").upper()
@@ -21,27 +22,46 @@ def init(time: int):
         elif ans == "E":
             record_earning(conn)
     elif action == "V":
-        view_spending(conn)
-    elif action == "M":
-        make_rec()
+        view_finance(conn)
     elif action == "Q":
+        print("QUITQ")
         sys.exit()
 
-    get_posts(conn)
-
 def create_table(conn):
+    """
+    Creates database tables to set up program
+    :param conn: connection for databases
+    :return: n/a (only creates table)
+    """
     cursor = conn.cursor()
-    sql = """
+    sql_expenses = """
         CREATE table IF NOT EXISTS expenses (
             datestr string,
             expense string,
             amount number
         )
     """
-    cursor.execute(sql)
+    cursor.execute(sql_expenses)
+    sql_earnings = """
+            CREATE table IF NOT EXISTS earnings (
+                datestr string,
+                expense string,
+                amount number
+            )
+        """
+    cursor.execute(sql_earnings)
     conn.commit()
 
-def record_spendings(conn):
+def to_dollar(amount: float):
+    """
+    Changes float to dollar currency format
+    :param amount: amount in $ to convert
+    :return: amount as a dollar value
+    """
+    dollar = '$' + str("{:,.2f}".format(amount))
+    return dollar
+
+def record_spending(conn):
     """
     A function for the user to input data about a spending
     :param conn: connection to the SQL database
@@ -66,12 +86,37 @@ def record_spendings(conn):
 
     init(1)
 
-def view_spending(conn):
+def record_earning(conn):
     """
-        A function for the user to see their total spending/earning
-        :param conn: connection to the SQL database
-        :return: n/a (prints spending data)
-        """
+    A function for the user to input data about an earning
+    :param conn: connection to the SQL database
+    :return: n/a (prints success)
+    """
+    cursor = conn.cursor()
+
+    print("-----")
+    print("Record Earning")
+    print("-----")
+
+    spending = input("Date of earning: ")
+    earning = input("Title of earning: ")
+    amount = input("Amount: $")
+
+    cursor.execute("insert into earnings values (?, ?, ?)", (spending, earning, amount))
+    conn.commit()
+
+    print("-----")
+    print("Earning for '" + earning + "' added!")
+    print("-----")
+
+    init(1)
+
+def view_finance(conn):
+    """
+    A function for the user to see their total spending/earning
+    :param conn: connection to the SQL database
+    :return: n/a (prints spending data)
+    """
     cursor = conn.cursor()
 
     print("-----")
@@ -84,26 +129,45 @@ def view_spending(conn):
     for expense in data:
         date = expense[0]
         title = expense[1]
-        amount = '$' + str('{:,.2f}'.format(expense[2]))
+        amount = to_dollar(expense[2])
+        print(amount + " for " + title + " on " + date)
+
+    total_spending = 0
+    for expense in data:
+        amount = expense[2]
+        total_spending += amount
+
+    print("-----")
+    print("Your Earning")
+    print("-----")
+
+    cursor.execute("select * from earnings")
+    data = cursor.fetchall()
+
+    for earning in data:
+        date = earning[0]
+        title = earning[1]
+        amount = to_dollar(earning[2])
         print(amount + " for " + title + " on " + date)
 
     print("-----")
 
-    total = 0
-    for expense in data:
-        amount = expense[2]
-        total += amount
+    total_earning = 0
+    for earning in data:
+        amount = earning[2]
+        total_earning += amount
 
-    print("Total Spending: $" + str(total))
+    net = total_earning - total_spending
+
+    total_spending = to_dollar(total_spending)
+    total_earning = to_dollar(total_earning)
+    print("Total Spending: " + total_spending)
+    print("Total Earning: " + total_earning)
+
+    print("Net: " + to_dollar(net))
     print("-----")
 
     init(1)
-
-
-def get_posts(conn):
-    cursor = conn.cursor()
-    cursor.execute("select * from expenses")
-    print(cursor.fetchall())
 
 
 init(0)
